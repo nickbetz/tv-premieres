f0c6a1d939a50c13d5bbead626072780eb711131
Nick Betz <nickb@criticalmass.com>
2015-05-23 17:38:45 -0600

    added gulp files

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  htmlify = require('gulp-angular-htmlify');
+
+gulp.task('connect', function() {
+  connect.server({
+  });
+});
+
+// gulp
+//     .src( 'main.css' )
+//     .pipe( checkCSS({
+//         files: '*.html'
+//     }))
+//     .on( 'error', function( err ) {
+//         // unnecessary since gulp-check-unused-css logs by default
+//         console.log( 'Unused classes:', err.unused );
+//     });
+
+gulp.task('htmlify', function() {
+    gulp.src('index.html')
+        .pipe(htmlify())
+        .pipe(gulp.dest(''));
+});
+
+gulp.task('default', ['connect']);
