8a7169605ac49498a97d2ec44454ce24ede2d92c
Nick Betz <nickb@criticalmass.com>
2015-06-16 15:42:16 -0600

    switched to sass and added some gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,16 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect'),
-  htmlify = require('gulp-angular-htmlify');
+  htmlify = require('gulp-angular-htmlify'),
+  sass = require('gulp-sass'),
+  useref = require('gulp-useref'),
+  minifyCss = require('gulp-minify-css'),
+  uglify = require('gulp-uglify'),
+  gulpIf = require('gulp-if');
 
 gulp.task('connect', function() {
   connect.server({
   });
 });
-
-// gulp
-//     .src( 'main.css' )
-//     .pipe( checkCSS({
-//         files: '*.html'
-//     }))
-//     .on( 'error', function( err ) {
-//         // unnecessary since gulp-check-unused-css logs by default
-//         console.log( 'Unused classes:', err.unused );
-//     });
 
 gulp.task('htmlify', function() {
     gulp.src('index.html')
@@ -23,4 +18,26 @@ gulp.task('htmlify', function() {
         .pipe(gulp.dest(''));
 });
 
-gulp.task('default', ['connect']);
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./styles'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('useref', function () {
+    var assets = useref.assets();
+    
+    return gulp.src('index.html')
+        .pipe(assets)
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['connect','sass:watch']);
